e6d619b5f80d31ae6d6dffa24f4589287ac8f2b4
AnasXilyas <164601125+AnasXilyas@users.noreply.github.com>
2024-05-02 18:24:16 +0500

    task done functionality added in app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
+def create_task_table():
+    conn = sqlite3.connect('taskHistory.db')
+    conn.execute('CREATE TABLE IF NOT EXISTS Task_History(task_name TEXT , task_duration INTEGER)')
+
+    conn.commit()
+    conn.close()
+
+
 @app.route('/')
 def home():
-
+    
     return render_template('index.html')
 
 @app.route('/insert', methods = ['POST', 'GET'])
@@ -16,7 +25,17 @@ def insert_task():
     
     return render_template('task.html')
    
+@app.route('/task_done')
+def task_done():
+    if 'task_name' in request.args and 'task_duration' in request.args:
+        conn = sqlite3.connect('taskHistory.db')
+        cur = conn.cursor()
+        cur.execute('INSERT INTO Task_History (task_name, task_duration) values (?,?)' , (request.args['task_name'] , request.args['task_duration']) )
+        conn.commit()
+        conn.close()
+    return "task_done"
+
 
 if __name__ == "__main__":
-
+    create_task_table()
     app.run(debug=True)
