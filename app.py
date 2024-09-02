ca0366f1bffdc5671b4c923a711593a231a490dc
M786453 <ahteshamsarwar333@gmail.com>
2024-09-02 23:19:16 +0500

    CORS issue fixed.

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app) # Enable cors for all routes
 
 bg_colors = [
               'rgba(255, 99, 132, 0.2)',
